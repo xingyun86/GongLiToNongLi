348647b8e4febfb3145a587e73b4c21d88e89899
xingyun86 <xingyun86>
2022-09-22 01:44:02 +0800

    提交优化

--- src/GongLiToNongLi/GL2NL/dllmain.cpp
+++ src/GongLiToNongLi/GL2NL/dllmain.cpp
@@ -10,197 +10,54 @@
 #else
 #define EXPORT_API  _declspec(dllexport)
 #endif
-//通用版将wstring转化为string
-__inline 
-std::string W_To_A(const std::wstring& ws, unsigned int cp = CP_ACP)
-{
-    if (!ws.empty())
-    {
-        std::string s(WideCharToMultiByte(cp, 0, ws.data(), -1, NULL, 0, NULL, NULL), ('\0'));
-        return s.substr(0, WideCharToMultiByte(cp, 0, ws.c_str(), -1, (LPSTR)s.data(), (int)s.size(), NULL, NULL) - 1);
-    }
-    return ("");
-}
-//通用版将string转化为wstring
-__inline 
-std::wstring A_To_W(const std::string& s, unsigned int cp = CP_ACP)
-{
-    if (!s.empty())
-    {
-        std::wstring ws(MultiByteToWideChar(cp, 0, s.data(), -1, NULL, 0), (L'\0'));
-        return ws.substr(0, MultiByteToWideChar(cp, 0, s.data(), -1, (LPWSTR)ws.data(), (int)ws.size()) - 1);
-    }
-    return (L"");
-}
-time_t date2uinx(const char(&date)[9])
-{
-    struct tm tm = { 0 };
-    sscanf(date, "%04d%02d%02d", &tm.tm_year, &tm.tm_mon, &tm.tm_mday);
-    tm.tm_year -= 1900;
-    tm.tm_mon--;
-    return mktime(&tm);
-}
-const char* unix2date(char(&date)[9], const time_t& t)
-{
-    memset(date, '\0', 9);
-    struct tm tm = { 0 };
-    localtime_s(&tm, &t);
-    strftime(date, 9, "%Y%m%d", &tm);
-    return date;
-}
-void GongLiToNongLi(
-    char(&lunar_hour)[5], 
-    char(&lunar_day)[5], 
-    char(&lunar_month)[9], 
-    char(&lunar_year_era)[7], 
-    char(&lunar_year_zodiac)[5], 
-    const char* calendar = "19980101")
-{
-    const char* m_TianGan[] = {
-        "甲",
-        "乙",
-        "丙",
-        "丁",
-        "戊",
-        "己",
-        "庚",
-        "辛",
-        "壬",
-        "癸",
-    };
-    const char* m_DiZhi[] = {
-        "子",
-        "丑",
-        "寅",
-        "卯",
-        "辰",
-        "巳",
-        "午",
-        "未",
-        "申",
-        "酉",
-        "戌",
-        "亥",
-    };
-    const char* m_ShuXiang[] = {
-        "鼠",
-        "牛",
-        "虎",
-        "兔",
-        "龙",
-        "蛇",
-        "马",
-        "羊",
-        "猴",
-        "鸡",
-        "狗",
-        "猪",
-    };
-    const int m_TimeSection[] = {
-        2300,59,
-        100,259,
-        300,459,
-        500,659,
-        700,859,
-        900,1059,
-        1100,1259,
-        1300,1459,
-        1500,1659,
-        1700,1859,
-        1900,2059,
-        2100,2259,
-    };
-    const char* m_DayName[] = {
-      "*",
-      "初一","初二","初三","初四","初五",
-      "初六","初七","初八","初九","初十",
-      "十一","十二","十三","十四","十五",
-      "十六","十七","十八","十九","二十",
-      "廿一","廿二","廿三","廿四","廿五",
-      "廿六","廿七","廿八","廿九","三十",
-    };
-    const char* m_MonthName[] = { "*",
-        "正","二","三","四","五","六","七","八","九","十","十一","腊",
-    };
-
-    Solar solar = { 0 };
-    char calendar_date[9] = { 0 }, czYear[5] = { 0 }, czMonth[3] = { 0 }, czDay[3] = { 0 };
-    memcpy(calendar_date, calendar, 8);
-    if (strlen(calendar) == 12)
-    {
-        char time_section[5] = { 0 };
-        char szDate[9] = { 0 };
-        memcpy(time_section, calendar + 8, 4);
-        int nTimeSection = atoi(time_section);
-        if (m_TimeSection[0] <= nTimeSection && nTimeSection < 2400)
-        {
-            sprintf(lunar_hour, "%s时", m_DiZhi[0]);
-            strcpy(calendar_date, unix2date(szDate, date2uinx(calendar_date) + 86400));
-        }
-        else
-        {
-            if (nTimeSection <= m_TimeSection[1] && nTimeSection >= 0)
-            {
-                sprintf(lunar_hour, "%s时", m_DiZhi[0]);
-            }
-            else
-            {
-                for (int i = 2; i < (int)sizeof(m_TimeSection) / sizeof(*m_TimeSection); i += 2)
-                {
-                    if (m_TimeSection[i] <= nTimeSection && nTimeSection <= m_TimeSection[i + 1])
-                    {
-                        sprintf(lunar_hour, "%s时", m_DiZhi[i / 2]);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    solar.solarYear = atoi((const char*)memcpy(czYear, calendar_date, 4));
-    solar.solarMonth = atoi((const char*)memcpy(czMonth, calendar_date + 4, 2));
-    solar.solarDay = atoi((const char*)memcpy(czDay, calendar_date + 6, 2));
-    Lunar lunar = CSolarLunarConverter::SolarToLunar(solar);
-
-    sprintf(lunar_day, "%s", m_DayName[lunar.lunarDay]);
-
-    if (lunar.lunarMonth < 1)
-    {
-        sprintf(lunar_month, "闰%s月", m_MonthName[-1 * lunar.lunarMonth]);
-    }
-    else
-    {
-        sprintf(lunar_month, "%s月", m_MonthName[lunar.lunarMonth]);
-    }
-    sprintf(lunar_year_era, "%s%s年", m_TianGan[((lunar.lunarYear - 4) % 60) % 10], m_DiZhi[((lunar.lunarYear - 4) % 60) % 12]);
-    sprintf(lunar_year_zodiac, "%s年", m_ShuXiang[((lunar.lunarYear - 4) % 60) % 12]);
-}
 
 extern "C" 
 {
-    EXPORT_API void __stdcall GL2NLA(char* p_lunar_hour,
+    EXPORT_API void __stdcall GL2NLA(char* p_lunar_hour_era,
         char* p_lunar_day,
+        char* p_lunar_day_era,
         char* p_lunar_month,
+        char* p_lunar_month_era,
         char* p_lunar_year_era,
         char* p_lunar_year_zodiac,
+        char* p_constell,
+        char* p_weekday,
         const char* p_calendar)
     {
-        char lunar_hour[5] = { 0 };
-        char lunar_day[5] = { 0 };
+        Solar solar = { 0 };
+        char lunar_hour_era[5] = { 0 };
+        char lunar_day[7] = { 0 };
+        char lunar_day_era[7] = { 0 };
         char lunar_month[9] = { 0 };
+        char lunar_month_era[7] = { 0 };
         char lunar_year_era[7] = { 0 };
-        char lunar_year_zodiac[5] = { 0 };
-        GongLiToNongLi(lunar_hour, lunar_day, lunar_month, lunar_year_era, lunar_year_zodiac, p_calendar);
-        if (p_lunar_hour != nullptr)
+        char lunar_year_zodiac[7] = { 0 };
+        char constell[7] = { 0 };
+        char weekday[7] = { 0 };
+
+        CalendarToSolar(&solar, p_calendar);
+        GongLiToNongLi(lunar_hour_era, lunar_day, lunar_day_era, lunar_month, lunar_month_era, lunar_year_era, lunar_year_zodiac, &solar);
+        GetConstell(constell, solar.solarMonth, solar.solarDay);
+        GetWeekDay(weekday, solar.solarYear, solar.solarMonth, solar.solarDay);
+        if (p_lunar_hour_era != nullptr)
         {
-            strcpy(p_lunar_hour, lunar_hour);
+            strcpy(p_lunar_hour_era, lunar_hour_era);
         }
         if (p_lunar_day != nullptr)
         {
             strcpy(p_lunar_day, lunar_day);
         }
+        if (p_lunar_day_era != nullptr)
+        {
+            strcpy(p_lunar_day_era, lunar_day_era);
+        }
         if (p_lunar_month != nullptr)
         {
             strcpy(p_lunar_month, lunar_month);
+        }
+        if (p_lunar_month_era != nullptr)
+        {
+            strcpy(p_lunar_month_era, lunar_month_era);
         }
         if (p_lunar_year_era != nullptr)
         {
@@ -210,31 +67,60 @@ extern "C"
         {
             strcpy(p_lunar_year_zodiac, lunar_year_zodiac);
         }
+        if (p_constell != nullptr)
+        {
+            strcpy(p_constell, constell);
+        }
+        if (p_weekday != nullptr)
+        {
+            strcpy(p_weekday, weekday);
+        }
     }
-    EXPORT_API void __stdcall GL2NLW(wchar_t* p_lunar_hour,
+    EXPORT_API void __stdcall GL2NLW(wchar_t* p_lunar_hour_era,
         wchar_t* p_lunar_day,
+        wchar_t* p_lunar_day_era,
         wchar_t* p_lunar_month,
+        wchar_t* p_lunar_month_era,
         wchar_t* p_lunar_year_era,
         wchar_t* p_lunar_year_zodiac,
+        wchar_t* p_constell,
+        wchar_t* p_weekday,
         const wchar_t* p_calendar)
     {
-        char lunar_hour[5] = { 0 };
-        char lunar_day[5] = { 0 };
+        Solar solar = { 0 };
+        char lunar_hour_era[5] = { 0 };
+        char lunar_day[7] = { 0 };
+        char lunar_day_era[7] = { 0 };
         char lunar_month[9] = { 0 };
+        char lunar_month_era[7] = { 0 };
         char lunar_year_era[7] = { 0 };
-        char lunar_year_zodiac[5] = { 0 };
-        GongLiToNongLi(lunar_hour, lunar_day, lunar_month, lunar_year_era, lunar_year_zodiac, W_To_A(p_calendar).c_str());
-        if (p_lunar_hour != nullptr)
+        char lunar_year_zodiac[7] = { 0 };
+        char constell[7] = { 0 };
+        char weekday[7] = { 0 };
+
+        CalendarToSolar(&solar, W_To_A(p_calendar).c_str());
+        GongLiToNongLi(lunar_hour_era, lunar_day, lunar_day_era, lunar_month, lunar_month_era, lunar_year_era, lunar_year_zodiac, &solar);
+        GetConstell(constell, solar.solarMonth, solar.solarDay);
+        GetWeekDay(weekday, solar.solarYear, solar.solarMonth, solar.solarDay);
+        if (p_lunar_hour_era != nullptr)
         {
-            wcscpy(p_lunar_hour, A_To_W(lunar_hour).c_str());
+            wcscpy(p_lunar_hour_era, A_To_W(lunar_hour_era).c_str());
         }
         if (p_lunar_day != nullptr)
         {
             wcscpy(p_lunar_day, A_To_W(lunar_day).c_str());
         }
+        if (p_lunar_day_era != nullptr)
+        {
+            wcscpy(p_lunar_day_era, A_To_W(lunar_day_era).c_str());
+        }
         if (p_lunar_month != nullptr)
         {
             wcscpy(p_lunar_month, A_To_W(lunar_month).c_str());
+        }
+        if (p_lunar_month_era != nullptr)
+        {
+            wcscpy(p_lunar_month_era, A_To_W(lunar_month_era).c_str());
         }
         if (p_lunar_year_era != nullptr)
         {
@@ -243,6 +129,14 @@ extern "C"
         if (p_lunar_year_zodiac != nullptr)
         {
             wcscpy(p_lunar_year_zodiac, A_To_W(lunar_year_zodiac).c_str());
+        }
+        if (p_constell != nullptr)
+        {
+            wcscpy(p_constell, A_To_W(constell).c_str());
+        }
+        if (p_weekday != nullptr)
+        {
+            wcscpy(p_weekday, A_To_W(weekday).c_str());
         }
     }
 }
--- src/GongLiToNongLi/GongLiToNongLi/GongLiToNongLi.cpp
+++ src/GongLiToNongLi/GongLiToNongLi/GongLiToNongLi.cpp
@@ -6,196 +6,62 @@
 #include <time.h>
 #include <SolarLunarConverter.h>
 
-time_t date2uinx(const char (&date)[9])
-{
-    struct tm tm = {0};
-    sscanf(date, "%04d%02d%02d", &tm.tm_year, &tm.tm_mon, &tm.tm_mday);
-    tm.tm_year -= 1900;
-    tm.tm_mon--;
-    return mktime(&tm);
-}
-const char* unix2date(char(&date)[9], const time_t& t)
-{
-    struct tm tm = { 0 };
-    memset(date, '\0', 9);
-    localtime_s(&tm, &t);
-    strftime(date, 9, "%Y%m%d", &tm);
-    return date;
-}
-void GongLiToNongLi(
-    char(&lunar_hour)[5],
-    char(&lunar_day)[5],
-    char(&lunar_month)[9],
-    char(&lunar_year_era)[7],
-    char(&lunar_year_zodiac)[5],
-    const char* calendar = "19980101")
-{
-    const char* m_TianGan[] = {
-        "甲",
-        "乙",
-        "丙",
-        "丁",
-        "戊",
-        "己",
-        "庚",
-        "辛",
-        "壬",
-        "癸",
-    };
-    const char* m_DiZhi[] = {
-        "子",
-        "丑",
-        "寅",
-        "卯",
-        "辰",
-        "巳",
-        "午",
-        "未",
-        "申",
-        "酉",
-        "戌",
-        "亥",
-    };
-    const char* m_ShuXiang[] = {
-        "鼠",
-        "牛",
-        "虎",
-        "兔",
-        "龙",
-        "蛇",
-        "马",
-        "羊",
-        "猴",
-        "鸡",
-        "狗",
-        "猪",
-    };
-    const int m_TimeSection[] = {
-        2300,59,
-        100,259,
-        300,459,
-        500,659,
-        700,859,
-        900,1059,
-        1100,1259,
-        1300,1459,
-        1500,1659,
-        1700,1859,
-        1900,2059,
-        2100,2259,
-    };
-    const char* m_DayName[] = {
-      "*",
-      "初一","初二","初三","初四","初五",
-      "初六","初七","初八","初九","初十",
-      "十一","十二","十三","十四","十五",
-      "十六","十七","十八","十九","二十",
-      "廿一","廿二","廿三","廿四","廿五",
-      "廿六","廿七","廿八","廿九","三十",
-    };
-    const char* m_MonthName[] = { "*",
-        "正","二","三","四","五","六","七","八","九","十","十一","腊",
-    };
-
-    Solar solar = { 0 };
-    char calendar_date[9] = { 0 }, czYear[5] = { 0 }, czMonth[3] = { 0 }, czDay[3] = { 0 };
-    memcpy(calendar_date, calendar, 8);
-    if (strlen(calendar) == 12)
-    {
-        char time_section[5] = { 0 };
-        char szDate[9] = { 0 };
-        memcpy(time_section, calendar + 8, 4);
-        int nTimeSection = atoi(time_section);
-        if (m_TimeSection[0] <= nTimeSection && nTimeSection < 2400)
-        {
-            sprintf(lunar_hour, "%s时", m_DiZhi[0]);
-            strcpy(calendar_date, unix2date(szDate, date2uinx(calendar_date) + 86400));
-        }
-        else
-        {
-            if (nTimeSection <= m_TimeSection[1] && nTimeSection >= 0)
-            {
-                sprintf(lunar_hour, "%s时", m_DiZhi[0]);
-            }
-            else
-            {
-                for (int i = 2; i < (int)sizeof(m_TimeSection) / sizeof(*m_TimeSection); i += 2)
-                {
-                    if (m_TimeSection[i] <= nTimeSection && nTimeSection <= m_TimeSection[i + 1])
-                    {
-                        sprintf(lunar_hour, "%s时", m_DiZhi[i / 2]);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    solar.solarYear = atoi((const char*)memcpy(czYear, calendar_date, 4));
-    solar.solarMonth = atoi((const char*)memcpy(czMonth, calendar_date + 4, 2));
-    solar.solarDay = atoi((const char*)memcpy(czDay, calendar_date + 6, 2));
-    Lunar lunar = CSolarLunarConverter::SolarToLunar(solar);
-    std::cout << lunar.lunarYear << lunar.lunarMonth << lunar.lunarDay << std::endl;
-
-    sprintf(lunar_day, "%s", m_DayName[lunar.lunarDay]);
-
-    if (lunar.lunarMonth < 1)
-    {
-        sprintf(lunar_month, "闰%s月", m_MonthName[-1 * lunar.lunarMonth]);
-    }
-    else
-    {
-        sprintf(lunar_month, "%s月", m_MonthName[lunar.lunarMonth]);
-    }
-    sprintf(lunar_year_era, "%s%s年", m_TianGan[((lunar.lunarYear - 4) % 60) % 10], m_DiZhi[((lunar.lunarYear - 4) % 60) % 12]);
-    sprintf(lunar_year_zodiac, "%s年", m_ShuXiang[((lunar.lunarYear - 4) % 60) % 12]);
-}
-
 int main(int argc, char ** argv)
 {
     std::cout << "Hello World!" << std::endl;
+    Solar solar;
+    //const char* calendar = "20210402";// 0001";
+    const char* calendar = "20140521";// 0001";// 0001";
 
-    const char* calendar = "20210402";// 0001";
-
-    char lunar_hour[5] = { 0 };
-    char lunar_day[5] = { 0 };
+    char lunar_hour_era[5] = { 0 };
+    char lunar_day[7] = { 0 };
+    char lunar_day_era[7] = { 0 };
     char lunar_month[9] = { 0 };
+    char lunar_month_era[7] = { 0 };
     char lunar_year_era[7] = { 0 };
-    char lunar_year_zodiac[5] = { 0 };
-    GongLiToNongLi(lunar_hour, lunar_day, lunar_month, lunar_year_era, lunar_year_zodiac, calendar);
-    std::cout << calendar << "---" << lunar_hour << "," << lunar_day << "," << lunar_month << "," << lunar_year_era << "," << lunar_year_zodiac << std::endl;
-    
-    HMODULE hModule = LoadLibrary(TEXT("GL2NL32"));
+    char lunar_year_zodiac[7] = { 0 };
+    char constell[7] = { 0 };
+    char weekday[7] = { 0 };
+
+    CalendarToSolar(&solar, calendar);
+    GongLiToNongLi(lunar_hour_era, lunar_day, lunar_day_era, lunar_month, lunar_month_era, lunar_year_era, lunar_year_zodiac, &solar);
+    std::cout << calendar << "---" << lunar_hour_era << "," << lunar_day << "," << lunar_day_era << "," << lunar_month << "," << lunar_month_era << "," << lunar_year_era << "," << lunar_year_zodiac << "," << GetConstell(constell, solar.solarMonth, solar.solarDay) << "," << GetWeekDay(weekday, solar.solarYear, solar.solarMonth, solar.solarDay) << std::endl;
+    HMODULE hModule = LoadLibraryW((sizeof(void*) == sizeof(double)) ? L"GL2NL64" : L"GL2NL32");
     if (hModule != nullptr)
     {
-        typedef void(__stdcall* PFN_GL2NLA)(char* lunar_hour, char* lunar_day, char* lunar_month, char* lunar_year_era, char* lunar_year_zodiac, const char* calendar);
+        typedef void(__stdcall* PFN_GL2NLA)(char* lunar_hour_era, char* lunar_day, char* lunar_day_era, char* lunar_month, char* lunar_month_era, char* lunar_year_era, char* lunar_year_zodiac, char* constell, char*weekday, const char* calendar);
         PFN_GL2NLA fnGL2NLA = (PFN_GL2NLA)GetProcAddress(hModule, "GL2NLA");
         if (fnGL2NLA != nullptr)
         {
-            const char* calendarA = "202104020001";
-
-            char lunar_hourA[5] = { 0 };
-            char lunar_dayA[5] = { 0 };
-            char lunar_monthA[9] = { 0 };
-            char lunar_year_eraA[7] = { 0 };
-            char lunar_year_zodiacA[5] = { 0 };
-            fnGL2NLA(lunar_hourA, lunar_dayA, lunar_monthA, lunar_year_eraA, lunar_year_zodiacA, calendarA);
-            std::cout << calendarA << "---" << lunar_hourA << "," << lunar_dayA << "," << lunar_monthA << "," << lunar_year_eraA << "," << lunar_year_zodiacA << std::endl;
-
+            const char* calendar = "201405211103";
+            char lunar_hour_era[5] = { 0 };
+            char lunar_day[7] = { 0 };
+            char lunar_day_era[7] = { 0 };
+            char lunar_month[9] = { 0 };
+            char lunar_month_era[7] = { 0 };
+            char lunar_year_era[7] = { 0 };
+            char lunar_year_zodiac[7] = { 0 };
+            char constell[7] = { 0 };
+            char weekday[7] = { 0 };
+            fnGL2NLA(lunar_hour_era, lunar_day, lunar_day_era, lunar_month, lunar_month_era, lunar_year_era, lunar_year_zodiac, constell, weekday, calendar);
+            std::cout << calendar << "---" << lunar_hour_era << "," << lunar_day << "," << lunar_day_era << "," << lunar_month << "," << lunar_month_era << "," << lunar_year_era << "," << lunar_year_zodiac << "," << constell << "," << weekday << std::endl;
         }
-        typedef void(__stdcall* PFN_GL2NLW)(wchar_t* lunar_hour, wchar_t* lunar_day, wchar_t* lunar_month, wchar_t* lunar_year_era, wchar_t* lunar_year_zodiac, const wchar_t* calendar);
+        typedef void(__stdcall* PFN_GL2NLW)(wchar_t* lunar_hour_era, wchar_t* lunar_day, wchar_t* lunar_day_era, wchar_t* lunar_month, wchar_t* lunar_month_era, wchar_t* lunar_year_era, wchar_t* lunar_year_zodiac, wchar_t* constell, wchar_t* weekday, const wchar_t* calendar);
         PFN_GL2NLW fnGL2NLW = (PFN_GL2NLW)GetProcAddress(hModule, "GL2NLW");
         if (fnGL2NLW != nullptr)
         {
-            const wchar_t* calendarW = L"202104020001";
-
-            wchar_t lunar_hourW[5] = { 0 };
-            wchar_t lunar_dayW[5] = { 0 };
-            wchar_t lunar_monthW[9] = { 0 };
-            wchar_t lunar_year_eraW[7] = { 0 };
-            wchar_t lunar_year_zodiacW[5] = { 0 };
-            fnGL2NLW(lunar_hourW, lunar_dayW, lunar_monthW, lunar_year_eraW, lunar_year_zodiacW, calendarW);
-            std::cout << calendarW << "---" << lunar_hourW << "," << lunar_dayW << "," << lunar_monthW << "," << lunar_year_eraW << "," << lunar_year_zodiacW << std::endl;
-
+            const wchar_t* calendar = L"201405211103";
+            wchar_t lunar_hour_era[5] = { 0 };
+            wchar_t lunar_day[7] = { 0 };
+            wchar_t lunar_day_era[7] = { 0 };
+            wchar_t lunar_month[9] = { 0 };
+            wchar_t lunar_month_era[7] = { 0 };
+            wchar_t lunar_year_era[7] = { 0 };
+            wchar_t lunar_year_zodiac[7] = { 0 };
+            wchar_t constell[7] = { 0 };
+            wchar_t weekday[7] = { 0 };
+            fnGL2NLW(lunar_hour_era, lunar_day, lunar_day_era, lunar_month, lunar_month_era, lunar_year_era, lunar_year_zodiac, constell, weekday, calendar);
+            std::cout << W_To_A(calendar) << "---" << W_To_A(lunar_hour_era) << "," << W_To_A(lunar_day) << "," << W_To_A(lunar_day_era) << "," << W_To_A(lunar_month) << "," << W_To_A(lunar_month_era) << "," << W_To_A(lunar_year_era) << "," << W_To_A(lunar_year_zodiac) << "," << W_To_A(constell) << "," << W_To_A(weekday) << std::endl;
         }
         FreeLibrary(hModule);
     }
